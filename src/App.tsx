1566d289e74870ee81527b03dd2e3b3874a6aa53
kozincov <kozincov1998@gmail.com>
2022-04-21 23:36:37 +0300

    added Header & Navbar to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Navbar/>
+        </div>
+    );
 }
 
 export default App;
