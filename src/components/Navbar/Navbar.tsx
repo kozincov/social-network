c6cfdbbae2fff70584e0f0222c94afda47bdcb9f
kozincov <kozincov1998@gmail.com>
2022-04-21 23:36:02 +0300

    created component Navbar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import s from "./Navbar.module.css"
+
+function Navbar() {
+    return (
+        <nav className={s.nav}>
+            <div className={s.item}>
+                <a href="">Profile</a>
+            </div>
+            <div className={s.item}>
+                <a href="">Message</a>
+            </div>
+            <div className={s.item}>
+                <a href="">News</a>
+            </div>
+            <div className={s.item}>
+                <a href="">Music</a>
+            </div>
+            <div className={s.item}>
+                <a href="">Settings</a>
+            </div>
+
+        </nav>
+    );
+}
+
+export default Navbar;
+
