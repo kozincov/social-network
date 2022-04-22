a4c116868ba4fc711c55d2c12dbceb94613f63b7
kozincov <kozincov1998@gmail.com>
2022-04-22 22:08:38 +0300

    created component Profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import './App.css';
+import './App.module.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
+import style from "./App.module.css"
+import Profile from "./components/Profile/Profile";
 
 function App() {
     return (
-        <div className="App">
+        <div className={style.app_wrapper}>
             <Header/>
             <Navbar/>
+            <Profile/>
         </div>
     );
 }
---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import s from "./Profile.module.css"
+
+function Profile() {
+    return (
+        <div className={s.content}>
+            <div>
+                <img src="https://www.shutterstock.com/blog/wp-content/uploads/sites/5/2019/07/Man-Silhouette.jpg"/>
+            </div>
+            <div>
+                ava+description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
+
