b90b8616da1a4d391103333d7fd903c7def0d7c2
kozincov <kozincov1998@gmail.com>
2022-04-22 22:09:33 +0300

    refactor style in Profile

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import s from "./Profile.module.css"
+import style from "./Profile.module.css"
 
 function Profile() {
     return (
-        <div className={s.content}>
+        <div className={style.content}>
             <div>
                 <img src="https://www.shutterstock.com/blog/wp-content/uploads/sites/5/2019/07/Man-Silhouette.jpg"/>
             </div>
