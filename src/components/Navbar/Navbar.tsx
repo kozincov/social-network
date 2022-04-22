72c38bff3baa12283e3593e73cf59aff91f3434e
kozincov <kozincov1998@gmail.com>
2022-04-22 22:10:27 +0300

    refactor style in Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,22 +1,22 @@
 import React from "react";
-import s from "./Navbar.module.css"
+import style from "./Navbar.module.css"
 
 function Navbar() {
     return (
-        <nav className={s.nav}>
-            <div className={s.item}>
+        <nav className={style.nav}>
+            <div className={style.item}>
                 <a href="">Profile</a>
             </div>
-            <div className={s.item}>
+            <div className={style.item}>
                 <a href="">Message</a>
             </div>
-            <div className={s.item}>
+            <div className={style.item}>
                 <a href="">News</a>
             </div>
-            <div className={s.item}>
+            <div className={style.item}>
                 <a href="">Music</a>
             </div>
-            <div className={s.item}>
+            <div className={style.item}>
                 <a href="">Settings</a>
             </div>
 
