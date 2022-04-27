6a46f9ba4699db09f614fded5f64265f3578833e
kozincov <kozincov1998@gmail.com>
2022-04-27 22:01:36 +0300

    dialog layout

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,23 @@ import style from './Dialogs.module.css'
 
 export const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={style.dialogs}>
+            <div className={style.dialogsItem}>
+                <div className={style.dialog}>
+                    Dima
+                </div>
+                <div className={style.dialog}>
+                    Nikita
+                </div>
+                <div className={style.dialog}>
+                    Anton
+                </div>
+            </div>
+            <div className={style.message}>
+                <div className={style.dialog}>Hi</div>
+                <div className={style.dialog}>what`s up</div>
+                <div className={style.dialog}>Yo</div>
+            </div>
         </div>
     );
 };
