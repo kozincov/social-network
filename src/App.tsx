8c42c518c2940275f3844aae075b77b26319b537
kozincov <kozincov1998@gmail.com>
2022-04-29 18:55:37 +0300

    refactoring imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import './App.module.css';
-import Header from "./components/Header/Header";
-import Navbar from "./components/Navbar/Navbar";
 import style from "./App.module.css"
-import Profile from "./components/Profile/Profile";
 import {Route, Routes} from "react-router-dom";
+import {Header, Music, Navbar, News, Settings} from "./components";
 import {Dialogs} from "./components/Dialogs/Dialogs";
-import {Settings} from "./components/Settings/Settings";
-import {Music} from "./components/Music/Music";
-import {News} from "./components/News/News";
+import Profile from "./components/Profile/Profile";
 
 function App() {
     return (
