fbfeb2f5bccb366b3aa509bfc8a8453382347771
kozincov <kozincov1998@gmail.com>
2022-04-30 23:49:00 +0300

    refactoring component MyPosts

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,15 +3,26 @@ import style from "./MyPosts.module.css"
 import Post from "./Post/Post";
 
 function MyPosts() {
+
+    let postData = [
+        {id: 1, message: 'hello', likesCount: 10},
+        {id: 2, message: `It's my first post`, likesCount: 10}
+    ]
+
     return (
-        <div>
-            My posts
-            <div className={style.item}>
-                New post
-            </div>
+        <div className={style.postBlock}>
+            <h3>My posts</h3>
             <div>
-                <Post message={'hello'} countLike={15}/>
-                <Post message={`It's my first post`} countLike={10}/>
+                <div>
+                    <textarea></textarea>
+                </div>
+                <div>
+                    <button>Add post</button>
+                </div>
+            </div>
+            <div className={style.posts}>
+                <Post message={postData[0].message} countLike={postData[0].likesCount}/>
+                <Post message={postData[1].message} countLike={postData[1].likesCount}/>
             </div>
         </div>
     );
